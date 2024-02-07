3f00589d4602d93f60f8f615ffb0f51b139c3675
Morgan Lemke <morganl2010@gmail.com>
2024-02-07 15:57:16 -0500

    Update changeSlug.js

--- changeSlug.js
+++ changeSlug.js
@@ -1,14 +1,9 @@
 // Select the target node (usually the body or a broader container)
 const targetNode = document.body;
-var firstClick = 0;
 
 // Create a MutationObserver instance
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-    	if(firstClick==0){
-    		console.log('Chat Button Clicked!');
-    		firstClick++;	
-    	}
         if (mutation.addedNodes.length) {
             const inputElement = document.querySelector('.composer__input__d6OQi');
             if (inputElement) {
