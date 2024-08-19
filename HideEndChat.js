68d873dbf31d363004b4f51751f18bcd0eb402b4
Morgan Lemke <morganl2010@gmail.com>
2024-08-19 11:35:38 -0400

    hide chat for RC

---
+++ HideEndChat.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the button by its class name
+    const optionsButton = document.querySelector('.footer__options__8RNsw');
+
+    // Check if the button exists
+    if (optionsButton) {
+        // Hide the button
+        optionsButton.style.display = 'none';
+    }
+});
